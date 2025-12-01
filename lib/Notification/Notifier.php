15226d3ca7c15259e8b54177189bf0b8868e43c9
Florian Ludwig <florian.ludwig@uninow.de>
2025-12-01 23:53:58 +0100

    chore: add appointment reminder system via notifications
    Add background job for sending automatic appointment reminders to users who haven't responded. Add new admin settings for configuring reminder timing (1-30 days before appointment) and enabling/disabling reminders. Add notification support through INotifier interface with localized messages. Update admin settings UI with reminder configuration section including notifications app status check. Add Danish

---
+++ lib/Notification/Notifier.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Attendance\Notification;
+
+use OCP\IURLGenerator;
+use OCP\L10N\IFactory;
+use OCP\Notification\INotification;
+use OCP\Notification\INotifier;
+
+class Notifier implements INotifier {
+	private IFactory $l10nFactory;
+	private IURLGenerator $urlGenerator;
+
+	public function __construct(
+		IFactory $l10nFactory,
+		IURLGenerator $urlGenerator
+	) {
+		$this->l10nFactory = $l10nFactory;
+		$this->urlGenerator = $urlGenerator;
+	}
+
+	public function getID(): string {
+		return 'attendance';
+	}
+
+	public function getName(): string {
+		return $this->l10nFactory->get('attendance')->t('Attendance');
+	}
+
+	public function prepare(INotification $notification, string $languageCode): INotification {
+		if ($notification->getApp() !== 'attendance') {
+			throw new \InvalidArgumentException('Unknown app');
+		}
+
+		$l = $this->l10nFactory->get('attendance', $languageCode);
+
+		switch ($notification->getSubject()) {
+			case 'appointment_reminder':
+				$parameters = $notification->getSubjectParameters();
+				$appointmentName = $parameters['name'] ?? 'Unknown';
+				$appointmentDate = $parameters['date'] ?? 'Unknown';
+				
+				$notification->setParsedSubject(
+					$l->t('%s on %s', [$appointmentName, $appointmentDate])
+				);
+				$notification->setParsedMessage(
+					$l->t('Please respond to the upcoming appointment!')
+				);
+				$notification->setIcon($this->urlGenerator->getAbsoluteURL(
+					$this->urlGenerator->imagePath('attendance', 'app-dark.svg')
+				));
+				
+				return $notification;
+
+			default:
+				throw new \InvalidArgumentException('Unknown subject');
+		}
+	}
+}
